3ac223552efe5432aadafe2d6bd79f98c9d99aa7
Aimurat1 <aimurat927@gmail.com>
2021-01-08 14:30:45 +0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    pred_prob = round(torch.max(model.predict(img)[2]).item()*100)
+    pred_prob = round(torch.max(learn.predict(img)[2]).item()*100)
     return JSONResponse({'result': str(prediction) + ' ' + str(pred_prob)})
 
 
