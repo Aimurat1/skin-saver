b61b4347b5fc6f8ed7313a4566ba60236d5dc2c2
Aimurat1 <aimurat927@gmail.com>
2021-01-08 23:31:00 +0500

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -54,7 +54,7 @@ function analyze() {
       }
         if(parseInt(response["percent"]) < 70){
           document.getElementById("alert-danger").style.display = "block";
-          $("#alert-danger").first().hide().fadeIn(200).delay(9000).fadeOut(1000, function () { $(this).remove(); }); 
+          $("#alert-danger").first().hide().fadeIn(200).delay(2000).fadeOut(9000, function () { $(this).css("display","none"); });
         }
     }
     el("analyze-button").innerHTML = "Analyze";
